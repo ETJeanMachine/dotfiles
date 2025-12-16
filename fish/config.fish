dd838d998913c67f2cc949902bc9f151c144b348
Eric Hamilton <ericjean873@gmail.com>
2025-12-16 13:46:46 -0800

    remove fish_config bc that's not how this works now lel

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,6 @@ set -gx EDITOR hx
 set -gx PATH $HOME/go/bin $PATH
 
 if status is-interactive
-    alias fish_config='$EDITOR ~/.config/fish/config.fish'
     alias fish_source='source ~/.config/fish/config.fish'
 
     starship init fish | source
