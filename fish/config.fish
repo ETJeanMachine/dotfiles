cfed6d80e5de5a5c8d265d8182225083dbdcaca2
Eric Hamilton <ericjean873@gmail.com>
2025-12-16 13:43:17 -0800

    changed interactive shell stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,9 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-set -x EDITOR hx
-
-alias fish_config='$EDITOR ~/.config/fish/config.fish'
-alias fish_source='source ~/.config/fish/config.fish'
-
+set -gx EDITOR hx
 set -gx PATH $HOME/go/bin $PATH
 
-starship init fish | source
+if status is-interactive
+    alias fish_config='$EDITOR ~/.config/fish/config.fish'
+    alias fish_source='source ~/.config/fish/config.fish'
+
+    starship init fish | source
+end
