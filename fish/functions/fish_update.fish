e392a96c6bb52fb5ff594d3cccbf065a8aa5fb41
Eric Hamilton <ericjean873@gmail.com>
2025-12-16 13:33:39 -0800

    update install script, added update function

---
+++ fish/functions/fish_update.fish
@@ -0,0 +1,17 @@
+function fish_update --description "Update fish config from dotfiles repo"
+    set -l dotfiles_dir ~/.local/share/dotfiles
+
+    if not test -d $dotfiles_dir
+        echo "Dotfiles repo not found at $dotfiles_dir"
+        echo "Please run the install script first."
+        return 1
+    end
+
+    echo "Pulling latest changes..."
+    git -C $dotfiles_dir pull
+
+    echo "Updating fish configuration..."
+    cp -r $dotfiles_dir/fish/. ~/.config/fish/
+
+    echo "Done! Restart your shell or run 'source ~/.config/fish/config.fish' to apply changes."
+end
--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Copy fish configuration files
-cp fish/config.fish ~/.config/fish
-cp fish/functions/fish_greeting.fish ~/.config/fish/functions/
+DOTFILES_DIR="$HOME/.local/share/dotfiles"
+REPO_URL="https://github.com/ETJeanMachine/dotfiles.git"
+
+# Clone or update dotfiles repo to a known location
+if [ -d "$DOTFILES_DIR/.git" ]; then
+    echo "Updating existing dotfiles repo..."
+    git -C "$DOTFILES_DIR" pull
+else
+    echo "Cloning dotfiles repo to $DOTFILES_DIR..."
+    mkdir -p "$HOME/.local/share"
+    rm -rf "$DOTFILES_DIR" 2>/dev/null
+    git clone "$REPO_URL" "$DOTFILES_DIR"
+fi
+
+# Copy fish configuration files (merges with existing, overwrites conflicts)
+mkdir -p ~/.config/fish
+cp -r "$DOTFILES_DIR/fish/." ~/.config/fish/
 
 # Install starship prompt
 echo "Installing starship..."
