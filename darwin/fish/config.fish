2b1a32707154ffea9061f9a924ce070df0c0a78a
Eric Hamilton <ericjean873@gmail.com>
2025-12-16 09:57:20 -0800

    added macos config.fish; added editor and simple aliases.

--- darwin/fish/config.fish
+++ darwin/fish/config.fish
@@ -7,6 +7,4 @@ set -x EDITOR hx
 alias fishconf='$EDITOR ~/.config/fish/config.fish'
 alias fishsrc='source ~/.config/fish/config.fish'
 
-set -gx PATH $HOME/go/bin $PATH
-
 starship init fish | source
