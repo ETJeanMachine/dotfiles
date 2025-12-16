85ec1c29dd3ef222658096f3d6ebfc76663acfe6
Eric Hamilton <ericjean873@gmail.com>
2025-12-16 14:16:54 -0800

    updated configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
 set -gx EDITOR hx
-set -gx PATH $HOME/go/bin $PATH
 
 if status is-interactive
     alias fish_source='source ~/.config/fish/config.fish'
 
-    fnm env --use-on-cd --shell fish | source
     starship init fish | source
 end
+
+# Optional addons (added via fish_update --add <addon>)
+# fnm:path
+# fnm:env
+# go:path
+# cargo:path
