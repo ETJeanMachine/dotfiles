c3ab3df72cff05df1f51d8acf9c6157e33783dbf
Eric Hamilton <ericjean873@gmail.com>
2026-01-29 09:46:31 -0800

    replaced sed with string replace for macos/linux compat

--- fish/functions/hx.fish
+++ fish/functions/hx.fish
@@ -8,14 +8,16 @@ function hx --description "Helix wrapper that syncs theme with OS dark/light mod
         set theme $light_theme
     end
 
-    # Update config.toml with theme using sed
+    # Update config.toml with theme
     mkdir -p (dirname $config_file)
     if test -f $config_file
         if grep -q '^theme = ' $config_file
-            sed -i "s/^theme = .*/theme = \"$theme\"/" $config_file
+            string replace -r '^theme = .*' "theme = \"$theme\"" <$config_file >$config_file.tmp
         else
-            sed -i "1i theme = \"$theme\"" $config_file
+            echo "theme = \"$theme\"" >$config_file.tmp
+            cat $config_file >>$config_file.tmp
         end
+        mv $config_file.tmp $config_file
     else
         echo "theme = \"$theme\"" >$config_file
     end
